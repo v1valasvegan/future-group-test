9f7dc5b2e731d5822946cc432f03c4ef2ab3811e
Victor Zhuravlev <mocambe@mail.ru>
2020-02-03 00:36:00 +0300

    Updates

---
+++ precache-manifest.1767a835f8e904985823e59dfe38dcd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68b0f392505cd04969c0f6ea39e5e40",
+    "url": "/github.io/future-group-test/index.html"
+  },
+  {
+    "revision": "a8d926975683a82e2d00",
+    "url": "/github.io/future-group-test/static/css/main.f350bf46.chunk.css"
+  },
+  {
+    "revision": "30cb08b4026a1319fcba",
+    "url": "/github.io/future-group-test/static/js/2.48b349ac.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/github.io/future-group-test/static/js/2.48b349ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8d926975683a82e2d00",
+    "url": "/github.io/future-group-test/static/js/main.08362a54.chunk.js"
+  },
+  {
+    "revision": "b2cb815ab2a7bc93cc64",
+    "url": "/github.io/future-group-test/static/js/runtime-main.c3c28e78.js"
+  }
+]);
